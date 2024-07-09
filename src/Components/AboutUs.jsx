3bbdd57b40fa90acc1ea8e2af5b640a3f49e0232
MSTripathii <154688309+MSTripathii@users.noreply.github.com>
2024-07-09 20:55:50 +0530

    minor

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutUs = () => {
   return (
     <div className='flex flex-col items-center my-10' id='aboutus'>
-      <h1 className='text-5xl lg:text-7xl my-20 font-extrabold'><span className='bg-gradient-to-r from-teal-200 to-teal-500 text-transparent bg-clip-text'>Astitva</span>4<span className='bg-gradient-to-r from-teal-200 to-teal-500 text-transparent bg-clip-text'>Animals</span></h1>
+      <h1 className='text-4xl lg:text-7xl my-20 font-extrabold'><span className='bg-gradient-to-r from-teal-200 to-teal-500 text-transparent bg-clip-text'>Astitva</span>4<span className='bg-gradient-to-r from-teal-200 to-teal-500 text-transparent bg-clip-text'>Animals</span></h1>
       <p className='text-3xl md:w-1/2 text-center'>We are a group of individual keen to help needed animals with our 100% efforts with any organization support.
       We are <span className='font-bold bg-gradient-to-r from-teal-200 to-teal-500 text-transparent bg-clip-text'>serving from past 3 years</span> without any rest, our team is consist of 6 members everyone is dedicated towards their work 
       with their full potential 
