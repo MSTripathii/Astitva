8a1f0c96fbb20d325b153e3d4dbb3142fa3d248e
Madhusudan Tripathi <154688309+MSTripathii@users.noreply.github.com>
2024-10-24 17:50:42 +0000

    db connect

--- Backend/src/connections/userDB.ts
+++ Backend/src/connections/userDB.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectUserDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || "");
+    const conn = await mongoose.connect("mongodb+srv://madhusudan12tripathi:hello@astitva.ww4gw.mongodb.net/?retryWrites=true&w=majority&appName=astitva");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,17 +4,22 @@ import connectUserDB from "./connections/userDB";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 
-
+// Load environment variables from .env file
 dotenv.config();
 
+// Connect to the MongoDB database
 connectUserDB();
 
+console.log('MongoDB URI:', process.env.MONGO_URI);  // Debugging output
+
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 app.use(bodyParser.json());
 
+// Use the auth router
+app.use(authRouter);
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-app.use(authRouter);
