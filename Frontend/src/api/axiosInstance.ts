0d335b6aa52b3ced19bf30d7b6ba5947d83ba533
Madhusudan Tripathi <154688309+MSTripathii@users.noreply.github.com>
2024-11-18 11:40:08 +0530

    Delete Frontend directory

--- Frontend/src/api/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-// api/axiosInstance.js
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
-  withCredentials: true,
-});
-
-export default axiosInstance;
